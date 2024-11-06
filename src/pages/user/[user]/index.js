fbfc43bb7bf5282be411f2a5b260f46ed69aa216
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2024-11-06 10:35:13 +0530

    router.push() object way to re direct to some link

--- src/pages/user/[user]/index.js
+++ src/pages/user/[user]/index.js
@@ -9,7 +9,16 @@ function Index() {
                 his is the root page, for [user] folder, if after reaching till [user]
                 folder , we not mention any file{" "}
             </p>
-            <button onClick={() => router.push(`/user/${router.query.user}/setting`)}>Click for settings</button>
+            <button
+                onClick={() =>
+                    router.push({
+                        pathname: `/user/[user]/setting`,
+                        query: { user: router.query.user },
+                    })
+                }
+            >
+                Click for settings
+            </button>
             <button onClick={() => router.push(`/`)}>Home Page</button>
         </div>
     );

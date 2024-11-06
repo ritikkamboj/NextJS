166dedfd681e9f459dc58290b8f7837c6c35e3d0
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2024-11-06 09:53:23 +0530

    we can create root file for [dynamic folder]

---
+++ src/pages/user/[user]/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Index() {
+    return (
+        <div>This is the root page, for [user] folder, if after reaching till [user] folder , we not mention any file </div>
+    )
+}
+
+export default Index

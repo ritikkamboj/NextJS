b4183b88cf040ad4f7390eaaa1d8b48706a0c81a
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2024-11-06 10:26:36 +0530

    router.push() to move to some other link

--- src/pages/user/[user]/index.js
+++ src/pages/user/[user]/index.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import { useRouter } from "next/router";
+import React from "react";
 
 function Index() {
+    const router = useRouter();
     return (
-        <div>This is the root page, for [user] folder, if after reaching till [user] folder , we not mention any file </div>
-    )
+        <div>
+            <p>
+                his is the root page, for [user] folder, if after reaching till [user]
+                folder , we not mention any file{" "}
+            </p>
+            <button onClick={() => router.push(`/user/${router.query.user}/setting`)}>Click for settings</button>
+            <button onClick={() => router.push(`/`)}>Home Page</button>
+        </div>
+    );
 }
 
-export default Index
+export default Index;

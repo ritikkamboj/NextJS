8a03a7a5deeaf33eca8135d73860065e9facc7ad
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2024-11-08 14:23:08 +0530

    learn static site rendering (SSR) till static/users index.js, working on how to implement SSR furthur in dynamic routing

---
+++ src/pages/static/users/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Index(props) {
+    return (
+        <div>
+            <p> Static site Rendering </p>
+
+            {
+                props.data.users.map(user => (<li key={user.id}>{user.firstName} {user.lastName}</li>))
+            }
+        </div>
+    )
+}
+
+export const getStaticProps = async () => {
+    const data = await (await fetch('https://dummyjson.com/users')).json();
+
+    return {
+        props: {
+            data: data
+        }
+    }
+
+
+}
+
+export default Index

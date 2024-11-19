33f613fe390ba49f2cc11972db19759c1916b8bd
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2024-11-19 14:18:44 +0530

    see the different ways to import images in our file

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,19 @@ import styles from "@/styles/Home.module.css";
 import Script from "next/script";
 import Link from "next/link";
 
+// import greenImg from './../../public/green.jpg'
 
 export default function Home() {
   return (
-    <>
-
-
-    </>
+    <div>
+      <Image
+        src={
+          "https://images.pexels.com/photos/6074050/pexels-photo-6074050.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+        }
+        width={200}
+        height={200}
+        alt="Geen tree image"
+      />
+    </div>
   );
 }
